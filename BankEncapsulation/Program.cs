e7938b923dc6cd13f35c8001702a0b6cc5f11fb6
Kamilah <k.muhammad95@gmail.com>
2023-01-10 12:41:17 -0600

    class exercise complete

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var account = new BankAccount();
+            Console.WriteLine("Enter deposit amount.");
+
+            var depositAmount = double.Parse(Console.ReadLine());
+
+            account.Deposit(depositAmount);
+
+            Console.WriteLine($"Thank you! Your new Account Balance is: ${account.GetBalance()}");
+            
         }
     }
 }
